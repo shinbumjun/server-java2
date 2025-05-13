99d6e0dd0ee6910797c51b9e5d1712dac675250e
shinbumjun <https://github.com/shinbumjun>
2025-05-13 12:45:03 +0900

    test: 포인트 결제 테스트 리팩토링 (피드백 반영)

--- src/main/java/kr/hhplus/be/server/domain/service/PointService.java
+++ src/main/java/kr/hhplus/be/server/domain/service/PointService.java
@@ -6,6 +6,6 @@ import kr.hhplus.be.server.application.point.PointResult;
 public interface PointService {
     PointResult chargePoints(PointCriteria criteria);  // 포인트 충전
     PointResult getPoints(PointCriteria criteria);  // 포인트 조회
-    PointResult usePoints(Long orderId, int amount);  // 포인트 결제
+    PointResult usePoints(Long userId, int amount);  // 포인트 결제
 }
 
